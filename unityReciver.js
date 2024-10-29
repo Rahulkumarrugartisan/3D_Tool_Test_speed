f05d4a023a29cbe53cafbede0339f8dc2715a5e1
Rahulkumarrugartisan <rahul.kumar@rugartisan.com>
2024-10-29 15:22:09 +0530

    Update unityReciver.js

--- unityReciver.js
+++ unityReciver.js
@@ -1,7 +1,7 @@
 function sendDataToUnity(data = "Hello from HTML!") {
     if (window.unityInstance) {
         setTimeout(() => {
-            unityInstance.SendMessage("Receiver", "ReceiveData", data);
+            unityInstance.SendMessage("Addressable_handler", "ReceiveData", data);
             console.log("Data sent to Unity:", data);
         }, 1000); // 1-second delay to ensure Receiver is loaded
     } else {
